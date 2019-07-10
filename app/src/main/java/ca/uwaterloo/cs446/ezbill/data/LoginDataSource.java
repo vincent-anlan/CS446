cad10161f5bfab4c73e16f0a15e86eaa40d0f0e7
anlan <anlan9933@gmail.com>
2019-07-10 13:24:03 -0400

    fixed3

--- app/src/main/java/ca/uwaterloo/cs446/ezbill/data/LoginDataSource.java
+++ app/src/main/java/ca/uwaterloo/cs446/ezbill/data/LoginDataSource.java
@@ -12,6 +12,7 @@ public class LoginDataSource {
     public Result<LoggedInUser> login(String username, String password) {
 
         try {
+
             // TO-DO: handle loggedInUser authentication
             LoggedInUser fakeUser =
                     new LoggedInUser(
