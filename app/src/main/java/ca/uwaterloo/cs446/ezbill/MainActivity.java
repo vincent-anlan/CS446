71c1d70a22fe7f7f2e52605103a861968c3cf6d9
Xueyao Yu <x224yu@edu.uwaterloo.ca>
2019-07-22 04:40:02 -0400

    small fix

--- app/src/main/java/ca/uwaterloo/cs446/ezbill/MainActivity.java
+++ app/src/main/java/ca/uwaterloo/cs446/ezbill/MainActivity.java
@@ -89,14 +89,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             ImgAddr = info.getString("image");
             //need a image view to add profile picture
         }
-
-//        //Update user info
-//        text = findViewById(R.id.User_info);
-//        text.setText("email:"+email+"\n"+"uid:"+uid+"\n"+"username:"+username+"\n"
-//        +"imageAddress:"+ImgAddr);
-
         model.setUserEmail(email);
-
         model.setProfilePhotoURL(ImgAddr);
         //Login End
 
@@ -105,20 +98,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
         FirebaseUser user = auth.getCurrentUser();
         String url= user.getPhotoUrl().toString();
         Log.d("000","URL:"+url);
-//        //Update user info
-//        text = findViewById(R.id.User_info);
-//        text.setText("email:"+email+"\n"+"uid:"+uid+"\n"+"username:"+username+"\n"
-//        +"imageAddress:"+ImgAddr);
-
-//        //Update user info
-//        text = findViewById(R.id.User_info);
-//        text.setText("email:"+email+"\n"+"uid:"+uid+"\n"+"username:"+username+"\n"
-//        +"imageAddress:"+ImgAddr);
 //        //Login End
-
-
-
-        model = Model.getInstance();
 
         spinner = (ProgressBar) findViewById(R.id.progressBar);
         spinner.setVisibility(View.VISIBLE);
